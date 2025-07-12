6f1153dae081c425de412d7d081c94658ef92d0d
Bek <mansurbek@MacBook-Air-Mansurbek.local>
2025-07-12 11:56:06 +0500

    example code

---
+++ src/components/inputs/InputsProps.ts
@@ -0,0 +1,26 @@
+import type { ChangeEvent, HTMLInputTypeAttribute, CSSProperties } from 'react';
+
+export type InputsProps = {
+    label?: string;
+    type?: HTMLInputTypeAttribute | string;
+    name?: string;
+    id?: string;
+    value?: string | number | readonly string[] | undefined;
+    placeholder?: string;
+    disabled?: boolean;
+    checked?: boolean;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+    className?: string;
+    labelClassName?: string;
+    autoFocus?: boolean;
+    required?: boolean;
+    accept?: string;
+    multiple?: boolean;
+    readOnly?: boolean;
+    maxLength?: number;
+    minLength?: number;
+    min?: number | string;
+    max?: number | string;
+    step?: number;
+    style?: CSSProperties;
+};
